42f508ad904d962b989407a44ca80bfd606d6aa2
Architekton <architekton350@gmail.com>
2019-03-26 21:45:38 +1100

    more utility aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -10,6 +10,7 @@ alias ar="cd $HOME/pr/ar"
 alias pl="cd $HOME/pr/pl"
 alias os="cd $HOME/pr/os"
 alias re="cd $HOME/pr/re"
+alias ec="cd $HOME/pr/ewr"
 
 alias csig='git log --show-signature'
 alias dprev='git diff HEAD~ HEAD'
@@ -20,7 +21,12 @@ alias ....="cd ../../.."
 alias .....="cd ../../../.."
 alias ......="cd ../../../../.."
 
+alias clrun="RUST_LOG=info cargo run"
+alias cbrun="RUST_BACKTRACE=1 cargo run"
+
 alias urldecode="python -c 'import urllib.parse; import sys; print(urllib.parse.unquote(sys.argv[1]))'"
+alias b64decode="python -c 'import base64; import sys; print(base64.b64decode(sys.argv[1]).decode())'"
+alias b64encode="python -c 'import base64; import sys; print(base64.b64encode(sys.argv[1].encode()).decode())'"
 
 function sd
 {
